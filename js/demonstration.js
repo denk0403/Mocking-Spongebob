8c9451751dcf24a9f40233bceaa2b5212b5434fc
Dennis Kats <denniskats43@gmail.com>
2020-05-26 08:49:55 -0400

    Bug fixes to debug tools

--- js/demonstration.js
+++ js/demonstration.js
@@ -1,9 +1,10 @@
 "use strict";
-(() => {
+document.addEventListener("DOMContentLoaded", () => {
 	/**
 	 * Timer
 	 */
 	let timer;
+	const caption = document.getElementById("caption");
 
 	const TEXT = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ut eleifend libero, vel placerat tortor. Sed eget odio a lectus dapibus ornare vel sed felis. Aliquam lobortis, nibh tristique fermentum egestas, elit anteiaculis nibh, quis condimentum lorem lacus non lorem. Maecenas ut libero quis lorem facilisis dignissim. Aenean vitae massa varius, aliquam tellus vel,imperdiet dolor. Morbi cursus lectus id tempor egestas. Aliquamauctor hendrerit nibh, nec congue massa viverra eget. Vestibulum risus nulla,ullamcorper nec ultriciessit amet, rhoncus vellibero. Duis posuere auctor egestas. Aliquam eleifend nunc id mi dictum fringilla.Aenean quis magna nec nulla aliquet pharetra. Aenean at felis a antecondimentum maximus. Pellentesque egestas est vitae rutrum ultricies. Ut vehicula nisi a ipsum viverra viverra. Phasellus orci velit, ultrices ut magnain, cursus dapibus nulla. Phasellus at commodo lorem. Suspendisse porta nisl porttitor interdum tincidunt. Aliquam odio diam, malesuada eget porta vel,facilisis vel neque. Nam congue, arcuat pellentesque sollicitudin, eroslectus rutrum est, vel auctor erat leo sed justo. Mauris ac purus mi. Vestibulum vitae nunc velit. Nullam auctor velit a lacus elementum pharetra. Integerfinibus et est ut varius. Proin a orci vitae nisl lacinia luctus. Aenean at molestie enim. Suspendisse`;
 
@@ -12,13 +13,13 @@
 		if (time < 200) {
 			throw new Error("This may be too fast for your computer");
 		}
-		document.getElementById("caption").value = "";
+		caption.value = "";
 		let index = 0;
 		return (timer = setInterval(() => {
 			if (index < str.length) {
-				document.getElementById("caption").focus();
-				document.getElementById("caption").value += str.charAt(index++);
-				document.getElementById("caption").dispatchEvent(new Event("input"));
+				caption.focus();
+				caption.value += str.charAt(index++);
+				caption.dispatchEvent(new Event("input"));
 			} else {
 				clearInterval(timer);
 			}
@@ -35,19 +36,26 @@
 	window.typeLoremIpsumFast = () => {
 		clearInterval(timer);
 		let str = TEXT;
-		document.getElementById("caption").value = "";
+		caption.value = "";
 		let index = 0;
 		return (timer = setInterval(() => {
 			if (index < str.length) {
-				document.getElementById("caption").focus();
-				let addition = str.substring(0, TEXT.length / 1);
-				str = str.substring(TEXT.length / 1);
-				document.getElementById("caption").value += addition;
-				document.getElementById("caption").dispatchEvent(new Event("input"));
+				caption.focus();
+				let addition = str.substring(0, TEXT.length / 50);
+				str = str.substring(TEXT.length / 50);
+				caption.value += addition;
+				caption.dispatchEvent(new Event("input"));
 			} else {
 				clearInterval(timer);
 			}
 		}, 200));
+	};
+
+	window.typeLoremIpsumInstant = () => {
+		clearInterval(timer);
+		caption.focus();
+		caption.value = TEXT;
+		caption.dispatchEvent(new Event("input"));
 	};
 
 	window.stopLoremIpsum = () => {
@@ -56,8 +64,7 @@
 
 	window.mockError = () => {
 		clearInterval(timer);
-		const input = document.getElementById("caption");
-		input.value = "؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁";
-		input.dispatchEvent(new Event("input"));
+		caption.value = "؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁؁";
+		caption.dispatchEvent(new Event("input"));
 	};
-})();
+});
