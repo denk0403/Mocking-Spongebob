1ba165dfc3b599d4732c6b753a2ea2099454b454
Dennis Kats <denniskats43@gmail.com>
2022-11-22 08:09:46 -0500

    small button spacing tweak

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Use a cacheName for cache versioning
-const cacheName = "mockSpongebob-v3";
+const cacheName = "mockSpongebob-v4";
 
 // Assets to be used for offline availability
 const staticAssets = [
